73ab60431129255a97db3933d15c9a3ba663cdd0
Spleef Dinamix <pepi15032000@gmail.com>
2018-06-08 12:18:07 +0300

    Added Lambada Expression.

---
+++ LambdaAndLinqMoreExercises/LambadaExpressions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambadaExpressions
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var lambadaExpressions = new Dictionary<string, Dictionary<string, string>>();
+
+            while (input != "lambada")
+            {
+                var parts = input
+                    .Split(new char[] { ' ', '.', '=', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length > 1)
+                {
+                    var selector = parts[0];
+                    var selectorObject = parts[1];
+                    var property = parts[2];
+
+                    if (!lambadaExpressions.ContainsKey(selector))
+                    {
+                        lambadaExpressions.Add(selector, new Dictionary<string, string>());
+                    }
+                    lambadaExpressions[selector][selectorObject] = property;
+                }
+                else if (parts[0] == "dance")
+                {
+                    lambadaExpressions = lambadaExpressions
+                        .ToDictionary(
+                        selector => selector.Key,
+                        selector => selector.Value
+                            .ToDictionary(
+                            selectorObject => selectorObject.Key,
+                            selectorObject => selectorObject.Key + '.' + selectorObject.Value
+                            ));
+                }
+
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
