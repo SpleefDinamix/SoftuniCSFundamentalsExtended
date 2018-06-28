4d8ae099bfdbadda570678c2c0a64010fb3921aa
Spleef Dinamix <pepi15032000@gmail.com>
2018-06-28 13:55:57 +0300

    ReDirectory

---
+++ FilesDirectoriesExceptions/ReDirectory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace ReDirectory
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] files = Directory.GetFiles("../../input");
+
+            foreach (var file in files)
+            {
+                var fileParts = file.Split('\\').Last().Split('.');
+                string fileName = String.Join(".", fileParts.Take(fileParts.Length - 1));
+                string fileExtension = fileParts.Skip(fileParts.Length - 1).ToArray()[0];
+
+                if (!Directory.Exists("../../output/" + fileExtension + "s"))
+                {
+                    Directory.CreateDirectory("../../output/" + fileExtension + "s");
+                }
+
+                File.Copy(file, "../../output/" + fileExtension + "s/" + fileName + '.' + fileExtension);
+            }
+        }
+    }
+}
