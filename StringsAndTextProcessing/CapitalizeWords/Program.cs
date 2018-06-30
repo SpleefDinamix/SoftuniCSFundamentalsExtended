77766cffe059ae74b31cbb58e5a73ac80933dfc5
Spleef Dinamix <pepi15032000@gmail.com>
2018-06-30 23:02:45 +0300

    More on Strings

---
+++ StringsAndTextProcessing/CapitalizeWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace CapitalizeWords
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sentence = Console.ReadLine();
+
+            while (sentence != "end")
+            {
+                CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+                TextInfo textInfo = cultureInfo.TextInfo;
+
+                var words = sentence
+                    .Split(new[] { ' ', ',', '.', '?', '!', '(', ')', '-', '+', '*', '/', '#', ':', ';', '=' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => textInfo.ToTitleCase(x))
+                    .ToArray();
+
+                Console.WriteLine(String.Join(", ",words));
+                sentence = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ StringsAndTextProcessing/SentenceSplit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SentenceSplit
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sentence = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            var parts = sentence
+                .Split(new[] { delimiter }, StringSplitOptions.None);
+
+            string result = "[" + String.Join(", ", parts) + "]";
+            Console.WriteLine(result);
+        }
+    }
+}
