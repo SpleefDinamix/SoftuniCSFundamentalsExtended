6020c1a633397cce2c16e90ff91ee57a77de29b9
Spleef Dinamix <pepi15032000@gmail.com>
2018-06-30 19:22:06 +0300

    JSON

---
+++ StringsAndTextProcessing/JSONStringify/Program.Student.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace JSONStringify
+{
+    public partial class Program
+    {
+        public class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public List<int> Grades { get; set; }
+        }
+    }
+}
---
+++ StringsAndTextProcessing/JSONStringify/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSONStringify
+{
+    public partial class Program
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var students = new List<Student>();
+
+            while (inputLine != "stringify")
+            {
+                var studentParams = inputLine
+                    .Split(new[] { '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var nameAndAge = studentParams[0]
+                    .Split(new[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var grades = new List<int>();
+                if (studentParams.Length > 1)
+                {
+                    grades = studentParams[1]
+                    .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+                }
+                    
+
+                students.Add(new Student()
+                {
+                    Name = nameAndAge[0],
+                    Age = int.Parse(nameAndAge[1]),
+                    Grades = grades
+                });
+
+                inputLine = Console.ReadLine();
+            }
+
+            var jBuilder = new StringBuilder();
+            jBuilder.Append("[");
+
+            foreach (var student in students)
+            {
+                jBuilder.Append("{");
+
+                var jStudent = String.Format("name:\"{0}\",age:{1},grades:[{2}]",
+                    student.Name, student.Age, String.Join(", ", student.Grades));
+                jBuilder.Append(jStudent);
+
+                jBuilder.Append("},");
+            }
+            jBuilder.Remove(jBuilder.Length - 1, 1);
+            jBuilder.Append("]");
+
+            Console.WriteLine(jBuilder.ToString());
+        }
+    }
+}
