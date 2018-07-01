d3db1f3ba2a14d41ea5c0c6c7386265359ba3e0f
Spleef Dinamix <pepi15032000@gmail.com>
2018-07-01 21:14:47 +0300

    Moar SSStrings.

---
+++ StringsTextProcessingMoreExercises/SerializeString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SerializeString
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            char[] uniqueChars = text.Distinct().ToArray();
+
+            var counter = new Dictionary<char, List<int>>();
+            Array.ForEach(uniqueChars, x => counter[x] = new List<int>());
+
+            foreach (var uChar in uniqueChars)
+            {
+                int lastIndex = text.IndexOf(uChar);
+                counter[uChar].Add(lastIndex);
+
+                while (true)
+                {
+                    int nextIndex = text.IndexOf(uChar, lastIndex + 1);
+                    if (nextIndex < 0)
+                    {
+                        break;
+                    }
+                    counter[uChar].Add(nextIndex);
+                    lastIndex = nextIndex;
+                }
+            }
+
+            counter
+                .ToList()
+                .ForEach(x => Console.WriteLine(
+                    x.Key + ":" + String.Join("/",x.Value)));
+        }
+    }
+}
---
+++ StringsTextProcessingMoreExercises/Stateless/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stateless
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var state = Console.ReadLine();
+            var memory = new Dictionary<string, string>();
+
+            while (state != "collapse")
+            {
+                var fiction = Console.ReadLine();
+                memory[state] = fiction;
+
+                state = Console.ReadLine();
+            }
+
+            var statelessList = new List<string>();
+
+            foreach (var memo in memory)
+            {
+                string statelessWord = ConvertWordToStatless(memo);
+                statelessList.Add(statelessWord);
+            }
+
+            PrintStatlessStrings(statelessList);
+        }
+
+        public static string ConvertWordToStatless(KeyValuePair<string, string> memo)
+        {
+            string modState = memo.Key;
+            string modFiction = memo.Value;
+
+            while (modFiction.Length > 1)
+            {
+                modState = modState.Replace(modFiction, String.Empty);
+                modFiction = modFiction.Remove(0, 1);
+                modFiction = modFiction.Remove(modFiction.Length - 1, 1);
+            }
+            if (modFiction.Length > 0)
+            {
+                modState = modState.Replace(modFiction, String.Empty);
+            }
+
+            return modState;
+        }
+
+        public  static void PrintStatlessStrings(List<string> statelessList)
+        {
+            statelessList
+                .ForEach(state =>
+                {
+                    if (state.Length > 0)
+                    {
+                        Console.WriteLine(state.Trim());
+                    }
+                    else
+                    {
+                        Console.WriteLine("(void)");
+                    }
+                });
+        }
+    }
+}
