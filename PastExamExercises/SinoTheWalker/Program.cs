edcff1968d48bbffa8e5c844f42172bc3a78edf2
Spleef Dinamix <pepi15032000@gmail.com>
2018-07-06 21:48:31 +0300

    Solving Past Exam Problems

---
+++ PastExamExercises/SinoTheWalker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SinoTheWalker
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var startTime = TimeSpan.Parse(Console.ReadLine());
+            int steps = int.Parse(Console.ReadLine());
+            int secondsPerStep = int.Parse(Console.ReadLine());
+
+            int secondsInADay = 24 * 60 * 60;
+            int totalSecondsNeeded = (int)(((double)secondsPerStep * steps) % secondsInADay);
+            var endTime = startTime.Add(new TimeSpan(0, 0, totalSecondsNeeded));
+            Console.WriteLine("Time Arrival: " + endTime.ToString(@"hh\:mm\:ss"));
+        }
+    }
+}
---
+++ PastExamExercises/SoftUniKaraoke/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniKaraoke
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var participants = Console.ReadLine()
+                    .Split(',')
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+            var availableSongs = Console.ReadLine()
+                    .Split(',')
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+            var inputLine = Console.ReadLine();
+            var participantsDict = new Dictionary<string, SortedSet<string>>();
+
+            while (inputLine != "dawn")
+            {
+                var parts = inputLine
+                    .Split(',')
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                string participant = parts[0];
+                string song = parts[1];
+                string award = parts[2];
+
+                if (participants.Contains(participant) && availableSongs.Contains(song))
+                {
+                    if (!participantsDict.Keys.Contains(participant))
+                    {
+                        participantsDict.Add(participant, new SortedSet<string>());
+                    }
+                    participantsDict[participant].Add(award);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            if (participantsDict.Keys.Count < 1)
+            {
+                Console.WriteLine("No awards");
+                return;
+            }
+
+            participantsDict = participantsDict
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var participantAward in participantsDict)
+            {
+                string participant = participantAward.Key;
+                var awards = participantAward.Value;
+
+                string nameAndAwardCount = $"{participant}: {awards.Count} awards";
+                Console.WriteLine(nameAndAwardCount);
+
+                foreach (var award in awards.OrderBy(x => x))
+                {
+                    Console.WriteLine("--" + award);
+                }
+            }
+        }
+    }
+}
